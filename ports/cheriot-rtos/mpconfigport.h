09fa016a1e8faa6404b878c97cd0a874629fa6df
Jacob Trevor <Jacob.Trevor234@gmail.com>
2024-07-22 13:52:59 +0100

    ports/cheriot-rtos: Added machine spi implementation.
    Signed-off-by: Jacob Trevor <Jacob.Trevor234@gmail.com>

--- ports/cheriot-rtos/mpconfigport.h
+++ ports/cheriot-rtos/mpconfigport.h
@@ -3,41 +3,47 @@
 
 #include <stdint.h>
 
+enum { SPI_MSB_FIRST, SPI_LSB_FIRST };
+
 #define RETURN_TWICE [[gnu::returns_twice]]
 
 // options to control how MicroPython is built
 
 // Use the minimal starting configuration (disables all optional features).
-#define MICROPY_CONFIG_ROM_LEVEL (MICROPY_CONFIG_ROM_LEVEL_CORE_FEATURES) // MINIMUM)
+#define MICROPY_CONFIG_ROM_LEVEL \
+    (MICROPY_CONFIG_ROM_LEVEL_CORE_FEATURES) // MINIMUM)
 
 // You can disable the built-in MicroPython compiler by setting the following
 // config option to 0.  If you do this then you won't get a REPL prompt, but you
 // will still be able to execute pre-compiled scripts, compiled with mpy-cross.
-#define MICROPY_ENABLE_COMPILER     (1)
+#define MICROPY_ENABLE_COMPILER (1)
 
-#define MICROPY_QSTR_EXTRA_POOL           mp_qstr_frozen_const_pool
-#define MICROPY_ENABLE_GC                 (1)
-#define MICROPY_HELPER_REPL               (1)
-#define MICROPY_MODULE_FROZEN_MPY         (1)
-#define MICROPY_ENABLE_EXTERNAL_IMPORT    (1)
-#define MICROPY_PY_SYS                    (1)
-#define MICROPY_PY_SYS_MAXSIZE            (1)
+#define MICROPY_QSTR_EXTRA_POOL mp_qstr_frozen_const_pool
+#define MICROPY_ENABLE_GC (1)
+#define MICROPY_HELPER_REPL (1)
+#define MICROPY_MODULE_FROZEN_MPY (1)
+#define MICROPY_ENABLE_EXTERNAL_IMPORT (1)
+#define MICROPY_PY_SYS (1)
+#define MICROPY_PY_SYS_MAXSIZE (1)
 #define SSIZE_MAX INT_MAX
-#define MICROPY_PY_SYS_ARGV               (0)
-#define MICROPY_PY_SYS_MODULES            (0)
-#define MICROPY_PY_IO                     (0)
-#define MICROPY_PY_BUILTINS_MEMORYVIEW    (1)
+#define MICROPY_PY_SYS_ARGV (0)
+#define MICROPY_PY_SYS_MODULES (0)
+#define MICROPY_PY_IO (0)
+#define MICROPY_PY_BUILTINS_MEMORYVIEW (1)
 
-#define MICROPY_LONGINT_IMPL        (MICROPY_LONGINT_IMPL_MPZ)
+#define MICROPY_LONGINT_IMPL (MICROPY_LONGINT_IMPL_MPZ)
 
 #define MICROPY_ALLOC_PATH_MAX (256)
 
 #define MICROPY_PY_MACHINE (1)
 #define MICROPY_PY_MACHINE_INCLUDEFILE "ports/cheriot-rtos/modmachine.c"
 #define MICROPY_PY_MACHINE_PIN_MAKE_NEW mp_pin_make_new
+#define MICROPY_PY_MACHINE_SPI (1)
+#define MICROPY_PY_MACHINE_SPI_MSB (SPI_MSB_FIRST)
+#define MICROPY_PY_MACHINE_SPI_LSB (SPI_LSB_FIRST)
 
 // Use the minimum headroom in the chunk allocator for parse nodes.
-#define MICROPY_ALLOC_PARSE_CHUNK_INIT    (16)
+#define MICROPY_ALLOC_PARSE_CHUNK_INIT (16)
 
 // avoid STORE_LOCAL violations with thread state
 #define MICROPY_PY_STATE_THREAD_HACK (1)
@@ -45,15 +51,16 @@
 // type definitions for the specific machine
 
 #include <stddef.h>
-typedef ssize_t mp_int_t; // must be pointer size -- or does it?
-typedef size_t mp_uint_t; // must be pointer size -- or does it?
+typedef ssize_t mp_int_t;  // must be pointer size -- or does it?
+typedef size_t mp_uint_t;  // must be pointer size -- or does it?
 
 typedef long mp_off_t;
 
 #define MICROPY_HEAP_SIZE (65536)
 #define MICROPY_MIN_USE_CHERIOT_A7 (1)
 #define alloca(size) __builtin_alloca(size)
-static __attribute__((unused)) __attribute__((always_inline)) size_t strnlen(const char *s, size_t maxlen) {
+static __attribute__((unused)) __attribute__((always_inline)) size_t
+strnlen(const char *s, size_t maxlen) {
     int i = 0;
     while (*s++ && ++i < maxlen) {
     }
